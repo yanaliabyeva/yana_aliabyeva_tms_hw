86d40e00132d5343806b2ec2f17dead65c0af0ad
yanaliabyeva <yanaliabyeva817@gmail.com>
2022-11-14 21:33:17 +0300

    HomeWork_6

--- src/lesson_6/lesson_6.java
+++ src/lesson_6/lesson_6.java
@@ -2,9 +2,9 @@ package lesson_6;
 
 public class lesson_6 {
     public static void main(String[] args) {
-        CreditCard card = new CreditCard(1,2);
-        CreditCard card2 = new CreditCard(10,6);
-        CreditCard card3 = new CreditCard(5,8);
+        CreditCard card = new CreditCard(1,86);
+        CreditCard card2 = new CreditCard(2,64);
+        CreditCard card3 = new CreditCard(3,59);
         System.out.println(card.toString());
         System.out.println(card2);
         System.out.println(card3);
